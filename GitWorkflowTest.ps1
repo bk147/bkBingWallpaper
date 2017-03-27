0bc43d62ce14b6ed82997dc93dc7d9555cc66595
bk147 <bkirk@live.dk>
2017-03-28 01:46:42 +0200

    Added a test ps

---
+++ GitWorkflowTest.ps1
@@ -0,0 +1,10 @@
+workflow bkTest01 {
+    param (
+        [string] $repository,
+        [string] $tag
+    )
+    $folder = "$env:tmp"
+#    Write-Information -MessageData $repository
+    "Hello there..."
+    
+}
